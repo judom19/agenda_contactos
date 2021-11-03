0e6cb760bd240783b822f1a0e68e1ad88dc59340
Julio Dominguez <judom19@gmail.com>
2021-11-02 21:47:37 -0600

    Update funciones.js

--- funciones.js
+++ funciones.js
@@ -35,7 +35,7 @@ const crearContacto = (parentNode, contacto, db) => {
     iconoBorrar.classList.add('material-icons', 'icono')
     iconoBorrar.onclick = () => {
         db.removeItem(contacto.id)
-        window.location.href = '/'
+        window.location="index.html"
     }
     divContacto.appendChild(nombreContacto)
     divContacto.appendChild(numeroContacto)
